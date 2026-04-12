0c60cf1025725ffe331872d8e946441a75372a18
E-merging-digital <jonathan@emergingdigital.be>
2026-04-12 17:21:28 +0200

    fix(ci): start local web server for BrowserTest smoke run

--- web/modules/custom/homepage_smoke_test/tests/src/Functional/HomepageRenderTest.php
+++ web/modules/custom/homepage_smoke_test/tests/src/Functional/HomepageRenderTest.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Drupal\Tests\homepage_smoke_test\Functional;
 
 use Drupal\Tests\BrowserTestBase;
+use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
 
 /**
  * Basic smoke test for homepage rendering.
  *
  * @group homepage_smoke_test
  */
+#[RunTestsInSeparateProcesses]
 final class HomepageRenderTest extends BrowserTestBase {
 
   /**

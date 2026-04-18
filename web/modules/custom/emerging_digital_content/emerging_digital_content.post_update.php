c720a00a677063d0f0f6545879a961374af025b5
E-merging-digital <jonathan@emergingdigital.be>
2026-04-18 14:16:25 +0200

    Nettoyer le doublon Home/Accueil du menu principal

--- web/modules/custom/emerging_digital_content/emerging_digital_content.post_update.php
+++ web/modules/custom/emerging_digital_content/emerging_digital_content.post_update.php
@@ -28,7 +28,7 @@ function emerging_digital_content_post_update_import_default_content(array &$san
   }
 
   \Drupal::configFactory()->getEditable('system.site')
-    ->set('page.front', '/accueil')
+    ->set('page.front', '/')
     ->save(TRUE);
 
   \Drupal::state()->set('system.maintenance_mode', 0);
@@ -43,7 +43,7 @@ function emerging_digital_content_post_update_main_navigation_links(array &$sand
   $storage = \Drupal::entityTypeManager()->getStorage('menu_link_content');
 
   $links = [
-    ['title' => 'Accueil', 'uri' => 'internal:/accueil', 'weight' => 0],
+    ['title' => 'Accueil', 'uri' => 'internal:/', 'weight' => 0],
     ['title' => 'Services', 'uri' => 'internal:/services', 'weight' => 1],
     ['title' => 'IA & Drupal', 'uri' => 'internal:/ia-drupal', 'weight' => 2],
     ['title' => 'Cas clients', 'uri' => 'internal:/cas-clients', 'weight' => 3],
@@ -81,4 +81,62 @@ function emerging_digital_content_post_update_main_navigation_links(array &$sand
   }
 
   return sprintf('%d main navigation links were created.', $created);
+}
+
+/**
+ * Normalizes main navigation homepage links in French and removes duplicates.
+ */
+function emerging_digital_content_post_update_main_navigation_home_link_cleanup(array &$sandbox): string {
+  $storage = \Drupal::entityTypeManager()->getStorage('menu_link_content');
+
+  $ids = \Drupal::entityQuery('menu_link_content')
+    ->accessCheck(FALSE)
+    ->condition('menu_name', 'main')
+    ->condition('title', ['Accueil', 'Home'], 'IN')
+    ->execute();
+
+  if (!$ids) {
+    return 'No homepage links found in main navigation.';
+  }
+
+  $links = $storage->loadMultiple($ids);
+  $kept_accueil = NULL;
+  $removed = 0;
+  $updated = 0;
+
+  foreach ($links as $link) {
+    $title = (string) $link->label();
+    $uri = (string) ($link->get('link')->first()->getValue()['uri'] ?? '');
+
+    if ($title === 'Home') {
+      $link->delete();
+      $removed++;
+      continue;
+    }
+
+    if ($title === 'Accueil') {
+      if ($kept_accueil) {
+        $link->delete();
+        $removed++;
+        continue;
+      }
+
+      $kept_accueil = $link->id();
+      if ($uri !== 'internal:/') {
+        $link->set('link', ['uri' => 'internal:/']);
+        $updated++;
+      }
+      if ((int) $link->get('weight')->value !== 0) {
+        $link->set('weight', 0);
+        $updated++;
+      }
+      $link->save();
+    }
+  }
+
+  \Drupal::configFactory()->getEditable('system.site')
+    ->set('page.front', '/')
+    ->save(TRUE);
+
+  return sprintf('%d homepage links removed, %d homepage settings updated.', $removed, $updated);
 }
